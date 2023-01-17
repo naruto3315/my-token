f5ad6fce2c3b2962dda103b5998d6980617a7d2a
Funter <786133863@qq.com>
2023-01-18 06:43:58 +0800

    feature: test simple mint

--- test/MyToken.test.js
+++ test/MyToken.test.js
@@ -1,10 +1,20 @@
-describe('MyToken', function () {
-    async
+const { expect } = require("chai")
 
-    it('deploy', async function () {
+describe('MyToken', function () {
+    it('Check balances', async function () {
+        // 1. deploy
         const myTokenContractFactory = await hre.ethers.getContractFactory('MyToken')
-        const myTokenContract = await myTokenContractFactory.deploy()
+        const mintTokenNumber = hre.ethers.BigNumber.from(10)
+        const myTokenContract = await myTokenContractFactory.deploy(mintTokenNumber)
         await myTokenContract.deployed()
-        console.log('Contract deployed to:', myTokenContract.address)
+
+        // 2. connect wallet and mint
+        const [_, otherAccount] = await hre.ethers.getSigners()
+        await myTokenContract.connect(otherAccount).mint()
+
+        // 3. check whether user balance change is right
+        //    or not
+        const balance = await myTokenContract.balanceOf(otherAccount.address)
+        expect(balance).to.equal(mintTokenNumber)
     })
 })
